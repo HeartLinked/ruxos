e96d66f2d6c18bf90f13ad497bb567a8a8beaba9
Ren HangQi <2572131118@qq.com>
2024-10-27 19:37:54 +0800

    fix doc

--- modules/ruxhal/src/virtio/virtio_hal.rs
+++ modules/ruxhal/src/virtio/virtio_hal.rs
@@ -15,9 +15,12 @@ use core::ptr::NonNull;
 use driver_virtio::VirtIoHal;
 use virtio_drivers::{BufferDirection, PhysAddr};
 
+/// virtio-hal struct define
 pub struct VirtIoHalImpl;
 
+/// virtio-hal trait implement
 unsafe impl VirtIoHal for VirtIoHalImpl {
+    /// Allocate DMA buffer
     fn dma_alloc(pages: usize, _direction: BufferDirection) -> (PhysAddr, NonNull<u8>) {
         let vaddr = if let Ok(vaddr) = global_allocator().alloc_pages(pages, 0x1000) {
             vaddr
@@ -29,22 +32,26 @@ unsafe impl VirtIoHal for VirtIoHalImpl {
         (paddr.as_usize(), ptr)
     }
 
+    /// Deallocate DMA buffer
     unsafe fn dma_dealloc(_paddr: PhysAddr, vaddr: NonNull<u8>, pages: usize) -> i32 {
         global_allocator().dealloc_pages(vaddr.as_ptr() as usize, pages);
         0
     }
 
+    /// Convert physical address to virtual address
     #[inline]
     unsafe fn mmio_phys_to_virt(paddr: PhysAddr, _size: usize) -> NonNull<u8> {
         NonNull::new(phys_to_virt(paddr.into()).as_mut_ptr()).unwrap()
     }
 
+    /// Share DMA buffer
     #[inline]
     unsafe fn share(buffer: NonNull<[u8]>, _direction: BufferDirection) -> PhysAddr {
         let vaddr = buffer.as_ptr() as *mut u8 as usize;
         virt_to_phys(vaddr.into()).into()
     }
 
+    /// Unshare DMA buffer
     #[inline]
     unsafe fn unshare(_paddr: PhysAddr, _buffer: NonNull<[u8]>, _direction: BufferDirection) {}
 }
