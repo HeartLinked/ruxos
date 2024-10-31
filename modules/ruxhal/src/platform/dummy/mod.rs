ba03b3d283f7f43f8aecac4211f12c2bf04e8757
Ren HangQi <2572131118@qq.com>
2024-10-31 14:14:33 +0800

    fix CI (doc).

--- modules/ruxhal/src/platform/dummy/mod.rs
+++ modules/ruxhal/src/platform/dummy/mod.rs
@@ -82,9 +82,6 @@ pub mod irq {
     /// The timer IRQ number.
     pub const TIMER_IRQ_NUM: usize = 0;
 
-    /// The Virtio-console IRQ number
-    pub const VIRTIO_CONSOLE_IRQ_NUM: usize = 0;
-
     /// Enables or disables the given IRQ.
     pub fn set_enable(irq_num: usize, enabled: bool) {}
 
